c2c52e434d014f5d76015313658f2bad6f525ca1
Lu <shi-peng.lu@hpe.com>
2018-04-17 09:36:56 +0800

    Signed-off-by: Lu <shi-peng.lu@hpe.com>

--- com/hpeu/WebTours/Pytest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-sys.path
-print(sys.path)
-print(os.path.split(os.path.split(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0])[0])[0])
-sys.path.append(os.path.split(os.path.split(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0])[0])[0])
-print(sys.path)
